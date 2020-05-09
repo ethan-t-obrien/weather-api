de3e356b913ddc8a1fcd43334a036e834b704ae7
ethan-t-obrien <ethanobrien1009@gmail.com>
2020-05-09 17:31:30 +1200

    finished weather app tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,12 @@ function App() {
   }
 
   return (
-    <div className="app">
+    <div className={
+      (typeof weather.main != "undefined")
+       ? ((weather.main.temp > 16)
+        ? 'app warm'
+         : 'app')
+          : 'app'}>
       <main>
         <div className="search-box">
           <input 
@@ -47,20 +52,22 @@ function App() {
             onKeyPress={search}
             />
         </div>
-
-        <div className="location-box">
-          <div className="location">New York City, Us</div>
-          <div className="date">{dateBuilder(new Date())}</div>
+        {(typeof weather.name != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°C
+            </div>
+            <div className="weather">
+              {weather.weather[0].main}
+            </div>
+          </div>    
         </div>
-        <div className="weather-box">
-          <div className="temp">
-            15'C
-          </div>
-          <div className="weather">
-            Sunny
-          </div>
-        </div>    
-        
+        ) : ('')}
       </main>
     </div>
   );
